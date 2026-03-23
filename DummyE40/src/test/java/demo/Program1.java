dd310636079ec2e6aff54f3eee4573dd7e8a7d10
amruta <raskondaamruta32@gamil.com>
2026-03-23 17:18:24 +0530

    commit

---
+++ DummyE40/src/test/java/demo/Program1.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Program1 {
+	public static void main(String[] args) {
+		System.out.println("helloooo");
+		System.out.println("helloooo");
+		System.out.println("helloooo");
+	}
+}
